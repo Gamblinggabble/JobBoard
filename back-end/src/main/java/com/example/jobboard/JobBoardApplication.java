72d8ca6b519688e3c4db2ff81f38d3369fcdb8fc
Gabriela Dimcheva <gabriela.dim_cheva@abv.bg>
2022-12-11 22:06:04 +0200

    Add H2 in-memory database configurations

--- back-end/src/main/java/com/example/jobboard/JobBoardApplication.java
+++ back-end/src/main/java/com/example/jobboard/JobBoardApplication.java
@@ -1,10 +1,15 @@
 package com.example.jobboard;
 
+import com.example.jobboard.repos.JobPostCategoryTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobBoardApplication {
+
+    @Autowired
+    public JobPostCategoryTypeRepository jobPostCategoryTypeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(JobBoardApplication.class, args);
---
+++ back-end/src/main/java/com/example/jobboard/repos/Candidate_AuthRepository.java
@@ -0,0 +1,9 @@
+package com.example.jobboard.repos;
+
+import com.example.jobboard.model.Candidate_Auth;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Candidate_AuthRepository extends JpaRepository<Candidate_Auth, Integer> {
+}
