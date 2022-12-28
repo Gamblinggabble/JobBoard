2699df965a509c8df9ac8b7585112dd6066fae57
simeon-code <simeon_k@live.com>
2022-12-28 15:39:17 +0200

    Added search component

--- front-end/jobBoardApplication/src/app/app.component.ts
+++ front-end/jobBoardApplication/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jobBoardApplication';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/api/hello').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
