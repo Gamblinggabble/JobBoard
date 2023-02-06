24ba2a94b3b2ac3e0759cf30183728c920d73919
Darina Popova <daritopopova@gmail.com>
2023-02-06 11:53:45 +0200

    rebase

---
+++ front-end/jobBoardApplication/src/app/registration-person-page/registration-person-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-person-page',
+  templateUrl: './registration-person-page.component.html',
+  styleUrls: ['./registration-person-page.component.scss']
+})
+export class RegistrationPersonPageComponent {
+
+}
