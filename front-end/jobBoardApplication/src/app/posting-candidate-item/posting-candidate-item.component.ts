3b187630eede8db1acfc01e91a09e0676376a468
simeon-code <simeon_k@live.com>
2023-02-07 21:36:08 +0200

    Added conf. pages, worked on the home page, added an empty list component

---
+++ front-end/jobBoardApplication/src/app/posting-candidate-item/posting-candidate-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posting-candidate-item',
+  templateUrl: './posting-candidate-item.component.html',
+  styleUrls: ['./posting-candidate-item.component.scss']
+})
+export class PostingCandidateItemComponent {
+
+}
